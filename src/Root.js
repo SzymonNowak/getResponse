a26517d43d3371d1379a26ce10cb1151e4630b28
SzymonNowak <nnja5@o2.pl>
2019-11-28 12:37:40 +0100

    router work fine

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Challenge from './views/Challenge';
+import Dashboard from './views/Dashboard';
+import Recipes from './views/Recipes';
+import { routes } from './routes/index';
+const Root = () => (
 
-const Root = () => {
-  return (
-    <div>
-      elo
-    </div>
-  )
-}
+    <BrowserRouter>
+
+          <Switch>
+            {/* <Route  exact path={routes.home} render={() => <Redirect to={routes.dashboard} /> }/> */}
+            <Route   path={routes.dashboard} component={Dashboard}/>
+
+
+            <Route  exact path={routes.challenge} component={Challenge}/>
+
+
+            <Route  exact path={routes.recipes} component={Recipes}/>
+
+
+          </Switch>
+
+    </BrowserRouter>
+
+);
 
 export default Root;
